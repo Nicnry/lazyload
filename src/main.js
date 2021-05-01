c594294f824791b948cdc3c17c33f9c14e5d297a
Nicolas <nicolas.henry@heig-vd.ch>
2021-05-01 18:36:18 +0200

    Check number to show

--- src/main.js
+++ src/main.js
@@ -1,21 +1,25 @@
 var cards = document.getElementsByClassName('card')
 var jump = 10
 var actualShow = 10
+var time = 1000
 
-for (let i = 0; i <= jump-1; i++) {
-  cards[i].classList.remove('hidden')
-}
+showCards()
 
-function showCards(start = 0, end = 9) {
-  for (let i = start; i <= end-1; i++) {
-    cards[i].classList.remove('hidden')
-  }
-  actualShow = end
+function showCards(time = 0, start = 0, end = 10) {
+  setTimeout(function(){ 
+    for (let i = start; i <= end-1; i++) {
+      cards[i].classList.remove('hidden')
+    }
+    actualShow = end
+  }, time);
 }
 
 window.onscroll = function(ev) {
-  console.log(actualShow, '-------------------------------')
   if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-      showCards(actualShow, actualShow+jump)
+    if (actualShow < cards.length-jump) {
+      showCards(time, actualShow, actualShow+jump)
+    } else {
+      showCards(time, actualShow, cards.length)
+    }
   }
 }
